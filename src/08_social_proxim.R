1938adc1e294e756ab644ee741fa271a368de1ed
ckelling <ckelling@vt.edu>
2017-11-30 12:20:08 -0500

    changing this to just proxim setup

--- src/08_social_proxim.R
+++ src/08_social_proxim.R
@@ -1,6 +1,6 @@
 ###
 ### Claire Kelling
-### Social Proximity Model and Setup
+### Social Proximity Setup
 ###
 ### Created 11/29/17 for preliminary social proximity setup and modeling
 ### 
@@ -30,20 +30,14 @@ load(file = "C:/Users/ckell/OneDrive/Penn State/Research/bdss_igert_project/data
 
 
 edgelist <- mi_lodes_det_agg[,c(1,2)]
-#edgelist <- edgelist[1:2,]
-
 edgelist <- as.matrix(edgelist)
 
 
-#need to create variable for rowname and then proceed from here
-#after creating rownames, then can proceed with this
-#need to create unique assignment from GEOID to rowname (from 1 to 1822)
+# I need to rescale the block groups so that their ID's are not so large
 bg_dat <- det_bg@data
 new_geoid <- cbind(1:1822,bg_dat$GEOID)
 new_geoid[,1] <- as.numeric(new_geoid[,1])
 new_geoid[,2] <- as.numeric(new_geoid[,2])
-#not working?
-new_geoid[,1] <- as.numeric(new_geoid[,1])
 
 new_el <- matrix(NA,nrow = nrow(edgelist),ncol = ncol(edgelist))
 
@@ -70,11 +64,97 @@ for(i in 1:nrow(mat)){
   mat[i,i] <- 0
 }
 
-test2 <- neig2nb(neig(mat01 = mat))
-class(test2)
-plot(test2)
+#I need to remove those entries such that there are no ACS information, in order for modeling to work
+det_bg$id <- row.names(det_bg)
+det_bg@data <- left_join(det_bg@data, full_dat, by = (GEOID = "GEOID"))
+no_acs <- which(is.na(det_bg$median_income))
+na_dat<- det_bg[which(is.na(det_bg$median_income)),]
+det_bg <- det_bg[-which(is.na(det_bg$median_income)),]
 
+#they are equal (checked!)
+#View(cbind(det_bg$GEOID, new_geoid[,2]))
+mat <- mat[-no_acs,-no_acs] #this is my new adjacency matrix where all info is complete
+
+#create nb object
+proxim_nb <- neig2nb(neig(mat01 = mat))
+test2 <- neig(mat01 = mat)
+
+#now I would like to plot the social proximity
 coords <- coordinates(det_bg)
 plot(det_bg, border = "gray")
-plot(test2, coords, pch = 19, cex = 0.6, add = TRUE)
+plot(proxim_nb, coords, pch = 1, cex = 0.6, add = TRUE)
 plot(na_dat, col= "red", density =50,add = TRUE, border = "gray")
+#plot(na_dat, col= "red", density =50, border = "gray")
+
+######################################################################################
+##### Now, I will make a subset of the social proximity data, to make a clearer plot,
+##### for presentation purposes
+######################################################################################
+# I will choose the first mention of a block group
+#incl <- runif(1000, 1, 192352)
+#need to include at least one from each block group
+#sub_el <- new_el[incl,]
+
+#removing ones with no blockgroups first
+rem <- c()
+for(i in 1:nrow(edgelist)){
+  if(i %% 1000 ==0){
+    print(i)
+    }
+  if(edgelist[i,1] %in% na_dat$GEOID){
+    rem <- c(rem,i)
+    #edgelist <- edgelist[-i,]
+  }
+  if(edgelist[i,2] %in% na_dat$GEOID){
+    rem <- c(rem,i)
+    #edgelist <- edgelist[-i,]
+  }
+}
+rem <- unique(rem)
+edgelist <- edgelist[-rem,]
+
+
+#need to redo edgelist
+new_el <- matrix(NA,nrow = nrow(edgelist),ncol = ncol(edgelist))
+
+for(i in 1:nrow(edgelist)){
+  new_el[i,1] <- as.numeric(new_geoid[which(edgelist[i,1] == new_geoid[,2]),1])
+  new_el[i,2] <- as.numeric(new_geoid[which(edgelist[i,2] == new_geoid[,2]),1])
+}
+length(unique(c(new_el[,1],new_el[,2])))
+
+col1 <- new_el[!duplicated(new_el[,1]),]
+col2 <- new_el[!duplicated(new_el[,2]),]
+sub_el_comp <- rbind(col1,col2)
+length(unique(c(sub_el_comp[,1], sub_el_comp[,2])))
+sub_el_comp <- unique(sub_el_comp)
+
+#create new adjacency matrix
+mat <- matrix(0, 1822, 1822)
+mat[sub_el_comp] <- 1
+sum(mat)
+
+#need to make this matrix symmetric
+for(i in 1:nrow(mat)){
+  for(j in 1:ncol(mat)){
+    if(mat[i,j] == 1){
+      mat[j,i] <- 1
+    }
+  }
+  #need to make diagonal null (not own neighbor, just establishing links)
+  mat[i,i] <- 0
+}
+sum(mat)
+mat <- mat[-no_acs,-no_acs]
+
+#create nb object
+proxim_nb2 <- neig2nb(neig(mat01 = mat))
+summary(proxim_nb2)
+
+#now I would like to plot the social proximity
+coords <- coordinates(det_bg)
+plot(det_bg, border = "gray", main = "Social Proximity \nNeighborhood Structure (Sample)")
+plot(proxim_nb2, coords, pch = 1, cex = 0.6, add = TRUE)
+plot(na_dat, col= "red", density =50,add = TRUE, border = "gray")
+
+#save(proxim_nb, file = "C:/Users/ckell/OneDrive/Penn State/Research/bdss_igert_project/data/working/proxim_nb.Rdata")
