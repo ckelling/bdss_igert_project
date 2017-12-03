a7d053121685fea20c7c0b9d9b953c79dfe2909f
ckelling <ckelling@vt.edu>
2017-12-03 10:58:31 -0500

    point process setup

---
+++ src/13_point_process_model.R
@@ -0,0 +1,17 @@
+###
+### Claire Kelling
+### Point Process Model
+###
+### Created 12/1/2017 to create a point process model for response time
+### 
+### 
+
+#Libraries
+
+
+
+
+#Need to refer back to original crime data to access response time
+
+
+#take complete cases (no NA cases)
