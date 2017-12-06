5cefe0eb4c199b22f0f58cbe879306fd8c46ac1e
ckelling <ckelling@vt.edu>
2017-12-06 13:10:29 -0500

    making the grid and fitting different variogram models

--- src/13_point_process_model.R
+++ src/13_point_process_model.R
@@ -15,18 +15,17 @@ library(maps)
 library(acs)
 library(tigris)
 library(spdep)
-library(classInt)
-library(fields)
 library(ggplot2)
 library(dplyr)
 library(ade4) 
-library(igraph) 
 
 #Need to refer back to original crime data to access response time
 #load in subsetted data- only data that has valid values for Lat/Long
 load(file = "C:/Users/ckell/OneDrive/Penn State/Research/bdss_igert_project/data/working/detroit_data.Rdata")
 #   shape file
 load(file = "C:/Users/ckell/OneDrive/Penn State/Research/bdss_igert_project/data/working/det_bg.Rdata")
+load("C:/Users/cek32/Desktop/OneDrive/Penn State/Research/bdss_igert_project/data/working/detroit_data.Rdata")
+load("C:/Users/cek32/Desktop/OneDrive/Penn State/Research/bdss_igert_project/data/working/det_bg.Rdata")
 detroit_data$Longitude1 <- detroit_data$Longitude
 detroit_data$Latitude1 <- detroit_data$Latitude
 
@@ -35,6 +34,10 @@ coordinates(detroit_data) <- ~Longitude1+Latitude1
 #proj4string(detroit_data) <- proj4string(det_bg)
 proj4string(detroit_data) <- CRS("+proj=longlat")
 det_bg <- spTransform(det_bg, CRS("+proj=longlat"))
+
+#make a grid for wayne county
+grid <- makegrid(det_bg, cellsize = 0.1)
+grid <- SpatialPoints(grid, proj4string = CRS(proj4string(det_bg)))
 
 #remove variables that I am not interested in
 length(which(is.na(detroit_data$`Total Response Time`)))
@@ -58,56 +61,60 @@ ploteqc <- function(spobj, z, breaks, ...){
 ## Plotting
 #deleting the cases with negative time........ (no concerns on the large positive end)
 detroit_data <- detroit_data[-which(detroit_data$`Total Response Time`<0),]
-length(unique(detroit_data@data[,c(5,6)]))
-View(head(detroit_data))
+#removing one outlier that is not in detroit
+detroit_data <- detroit_data[-which(detroit_data$Longitude < -83.5),]
 
-range(detroit_data$`Total Response Time`)
-breaks <- 0:1000
-ploteqc(detroit_data, detroit_data$`Total Response Time`, breaks, pch = 19)
-map("county", region = "california", add = TRUE)
-title(main = "Average Annual Temperatures, 1961-1990, Degrees F")
+#I'm going to randomly select 1,000 points to plot
+plot_ind <- runif(n=3000, min=0, max=479100)
+plot_dat <- detroit_data[plot_ind,]
 
-dim(CAgrid)
-dim(CAtemp)
-
-#range(CAgrid$elevation)
-breaks <- seq(-100, 3600, by = 100)
-ploteqc(CAgrid, CAgrid$elevation, breaks, pch = 19)
-map("county", region = "california", add = TRUE)
-title(main = "Elevations at prediction locations, m")
+range(plot_dat$`Total Response Time`)
+breaks <- 0:range(plot_dat$`Total Response Time`)[2]
+xlim <- c(as.numeric(min(det_bg@data$INTPTLON)), as.numeric(max(det_bg@data$INTPTLON))) 
+xlim <- c(xlim[2], xlim[1])
+ylim <- c(as.numeric(min(det_bg@data$INTPTLAT)), as.numeric(max(det_bg@data$INTPTLAT))) 
+ploteqc(plot_dat, plot_dat$`Total Response Time`, breaks, pch = 19, xlim=xlim, ylim = ylim)
+plot(det_bg, add = TRUE)
+title(main = "Response Times, Wayne County")
 
 ##
-# Problem 2a
 # creating preliminary estimate
 ##
-#head(CAtemp)
+
+#recoding officer initiated into 0-1 from "No"/"Yes"
+detroit_data$`Officer Initiated`[which(detroit_data$`Officer Initiated` == "Yes")] <- 1
+detroit_data$`Officer Initiated`[which(detroit_data$`Officer Initiated` == "No")] <- 0
+detroit_data$`Officer Initiated` <- as.numeric(detroit_data$`Officer Initiated`)
+
+#trying the below calculations on a random subset of the data
+plot_ind <- runif(n=3000, min=0, max=479100)
+detroit_data <- detroit_data[plot_ind,]
 
 #Fitting the preliminary linear model
-linmod <- lm(avgtemp~lon+lat+elevation, data=CAtemp)
+linmod <- lm(`Total Response Time` ~`Officer Initiated`+Priority + Longitude + Latitude, data=detroit_data@data)
 summary(linmod)
 #storing the residuals so I can plot them
-CAtemp$resid <- linmod$resid
+detroit_data$resid <- linmod$resid
 #storing the coefficients for later
 beta_lin <- linmod$coefficients
 beta_lin
 
 #plotting the residuals
-#range(CAtemp$resid)
-breaks <- seq(-7, 7, by = 0.1)
-ploteqc(CAtemp, CAtemp$resid, breaks, pch = 19)
-map("county", region = "california", add = TRUE)
-title(main = "Residuals")
+range(detroit_data$resid)
+breaks <- seq(range(detroit_data$resid)[1], range(detroit_data$resid)[2], by = 10)
+ploteqc(detroit_data, detroit_data$resid, breaks, pch = 19, xlim = xlim, ylim = ylim)
+plot(det_bg, add= TRUE)
+title(main = "Residuals of Linear Model")
 
 
 ##
-# Problem 2b
+# Variogram Estimation
 ##
-
 
 ##  Nonparametric estimation of the variogram
-vg <- variogram(resid ~ 1, data = CAtemp)#, width=75)
+vg <- variogram(resid ~ 1, data = detroit_data)#, width=75)
 print(vg)
-#plot(vg, xlab = "Distance", ylab = "Nonparametric Semi-variogram estimate", width=5)
+plot(vg, xlab = "Distance", ylab = "Nonparametric Semi-variogram estimate", width=5)
 
 ##  Fitting the variogram parametrically
 fitvg <- fit.variogram(vg, vgm(1, "Exp", range=200, nugget=1), fit.method = 2)
@@ -129,7 +136,7 @@ plot(vg, fitvg, xlab = "Distance", ylab = "Parametric and nonparametric Semi-var
 # between pairs of locations in CAtemp.
 #   There are some entries on the diagonal that are not zero, but this is also true
 #   in the case in the documentation, so I won't worry too much about it!
-d <- rdist.earth(coordinates(CAtemp), miles = TRUE)
+d <- rdist.earth(coordinates(detroit_data), miles = TRUE)
 
 # Create the covariance matrix, plugging in your estimates from the fitted variogram.
 # add two matrices together
@@ -144,31 +151,31 @@ cov <- mat1+mat2
 Sinv <- solve(cov)
 
 # Create the X matrix. Hint: Use cbind.
-n <- nrow(CAtemp)
-m <- nrow(CAgrid)
-X <- cbind(rep(1,n), CAtemp$lon, CAtemp$lat, CAtemp$elevation)
+n <- nrow(detroit_data)
+m <- nrow(detroit_data)
+X <- cbind(rep(1,n), detroit_data$`Officer Initiated`, detroit_data$Priority, detroit_data$Longitude, detroit_data$Latitude)
 
 # Put all the pieces together to form $\hat{\beta}_{GLS}$
 
-Y <- CAtemp$avgtemp
+Y <- detroit_data$`Total Response Time`
 X <- as.matrix(X)
 first <- t(X)%*%Sinv%*%X
 second <- t(X)%*%Sinv%*%Y
 beta_gls <- solve(first)%*%second
 
 # include estimates below
-rownames(beta_gls) <- c("Intercept", "lon", "lat", "elevation")
+rownames(beta_gls) <- c("Intercept", "Officer Initiated", "Priority", "Long", "Lat")
 t(beta_gls)
 
 beta_lin
 
 ##
-# Problem 2d
+# Prediction
 ##
 
-dcross <- rdist.earth(coordinates(CAtemp), coordinates(CAgrid))
-dpred <- rdist.earth(coordinates(CAgrid))
-Xpred <- cbind(rep(1,m), CAgrid$lon, CAgrid$lat, CAgrid$elevation)
+dcross <- rdist.earth(coordinates(detroit_data), coordinates(detroit_data))
+dpred <- rdist.earth(coordinates(detroit_data))
+Xpred <- cbind(rep(1,m), detroit_data$`Officer Initiated`, detroit_data$Priority, detroit_data$Longitude, detroit_data$Latitude)
 
 #Construct the covariance matrixes
 Gamma <- exp(-d/rho.hat)
@@ -181,7 +188,7 @@ y_pred <- Xpred%*%beta_gls + t(g)%*%Ginv%*%(Y-X%*%beta_gls)
 # Plot
 #range(y_pred)
 breaks <- seq(30, 80, by = 5)
-ploteqc(CAgrid, y_pred, breaks, pch = 19)
+ploteqc(detroit_data, y_pred, breaks, pch = 19)
 map("county", region = "california", add = TRUE)
 title(main = "Predicted Temperatures")
 
@@ -197,12 +204,12 @@ se_pred <- sqrt(v_pred)
 
 #range(se_pred)
 breaks <- seq(1.9, 2.1, by = 0.01)
-ploteqc(CAgrid, se_pred, breaks, pch = 19)
+ploteqc(detroit_data, se_pred, breaks, pch = 19)
 map("county", region = "california", add = TRUE)
 title(main = "Standard Error")
 
 breaks <- seq(1.9, 2.1, by = 0.01)
-ploteqc(CAgrid, se_pred, breaks, pch = 19)
+ploteqc(detroit_data, se_pred, breaks, pch = 19)
 map("county", region = "california", add = TRUE)
 title(main = "Standard Error with Observed Points")
 points(CAtemp)
