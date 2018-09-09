7f2a6f4e9b2bf94c5bf32f152b30bb6f18a03066
ckelling <ckelling@vt.edu>
2018-09-09 13:09:00 -0400

    initial commit

---
+++ src/00_modeling_functions.R
@@ -0,0 +1,19 @@
+###
+### Claire Kelling
+### Creating functions for the modeling used in 20_sensitivity_analysis.R
+### Includes functions for the modeling procedure and storage of output.
+###
+### Created       9/9/18
+### Last Modified 9/9/18
+### 
+
+
+## Modeling Procedure function:
+
+
+
+
+
+
+
+## Output storage function:
